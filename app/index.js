dfd2e3fa0be779076f9b66489226d82f54b53010
Marc Rousavy <me@mrousavy.com>
2024-05-07 15:55:12 +0200

    fix: Fix default import

--- app/index.js
+++ app/index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from '.';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
